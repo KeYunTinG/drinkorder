662218d58c4e6eab3c9e380bddf3921e23b417a8
KeYunTinG <zaac2240632@yahoo.com.tw>
2023-05-21 23:28:25 +0800

    Commodity2

--- controllers/commodity-controller.js
+++ controllers/commodity-controller.js
@@ -14,40 +14,14 @@ const commodityController = {
       nest: true
     })
       .then(commodities => {
-        console.log(req.body.drinkId0)
-        console.log(req.body.drinkId)
-        if (req.body.drinkId0) {
-          commodities = commodities.filter(Commodity => Commodity.commodityId == req.body.drinkId0)
-          console.log(0)
-        } else {
-          commodities = commodities.filter(Commodity => Commodity.commodityId == req.body.drinkId)
-          console.log(1)
-        }
+        commodities = commodities.filter(Commodity => Commodity.commodityId == req.body.drinkId)
         console.log(commodities)
-        res.render('cupselect', { commodities })
+        res.render('confirm', { commodities })
       })
       .catch((err) => {
         return res.redirect('back')
       })
   },
-  //   postCommodities: (req, res) => {
-  //     //const CommodityId = req.params.id.
-  //     console.log(req.body.flexRadioDefault)
-  //     Cupsize.findAll({
-  //       include: Commodity,
-  //       order: [['createdAt', 'DESC']],
-  //       raw: true,
-  //       nest: true
-  //     })
-  //       .then(Cupsizes => {
-  //         Cupsizes = Cupsizes.filter(Cupsize => Cupsize.CommodityId == req.params.id)
-  //         console.log(Cupsizes)
-  //         res.render('cupselect', { Cupsizes })
-  //       })
-  //       .catch((err) => {
-  //         return res.redirect('back')
-  //       })
-  //   },
 }
 
 module.exports = commodityController
