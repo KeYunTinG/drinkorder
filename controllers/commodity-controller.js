2bd319f7379018f4a169952dc4b715bcb733243a
zaac2240632 <zaac2240632@yahoo.com.tw>
2023-05-23 15:43:05 +0800

    confirm

--- controllers/commodity-controller.js
+++ controllers/commodity-controller.js
@@ -9,29 +9,63 @@ const commodityController = {
     res.render('starbuck', { time: timeList.products })
   },
   postCommodities: (req, res) => {
-    //待確認此人是否點過
-    //console.log(req.body.drinkId)
-    console.log(req.body.username)
     if (req.body.drinkId) {
+      const username = req.body.username.filter(element => element.length > 0)
       const orderItem = timeList.products.filter(TimeList => TimeList.product_variations.some(f => f.id == req.body.drinkId))
       const orderItemSize = orderItem[0].product_variations
-      orderItemSize.forEach(element => {
-        if (element.id == req.body.drinkId) {
-          //待確認此人是否點過
-          Order.create({
-            guest: req.body.username,
-            name: orderItem[0].name,
-            size: element.name,
-            cost: element.price,
-            commodityId: element.id,
+      //確認此人是否點過
+      Order.findOne({ where: { guest: username[0] } }).then(orders => {
+        if (orders) {
+          const id = orders.id
+          orderItemSize.forEach(element => {
+            if (element.id == req.body.drinkId) {
+              orders.update({
+                name: orderItem[0].name,
+                size: element.name,
+                cost: element.price,
+                commodityId: element.id,
+              })
+                .then(() => {
+                  return res.redirect(`/confirm/${orders.id}`);
+                })
+                .catch(() => {
+                  return res.redirect('/starbuck');
+                })
+            }
+          })
+        } else {
+          orderItemSize.forEach(element => {
+            if (element.id == req.body.drinkId) {
+              Order.create({
+                guest: username[0],
+                name: orderItem[0].name,
+                size: element.name,
+                cost: element.price,
+                commodityId: element.id,
+              })
+                .then(() => {
+                  return res.redirect(`/confirm/${orders.id}`);
+                })
+                .catch(() => {
+                  return res.redirect('/starbuck');
+                })
+            }
           })
         }
       })
-      return res.redirect('/confirm')
     } else {
       res.redirect('back')
     }
+  },
+  getConfirm: (req, res) => {
+    Order.findOne({ where: { id: req.params.id } }).then(orders => {
+      res.render('confirm', { guest: orders.guest, name: orders.name })
+    })
+      .catch(() => {
+        return res.redirect('/starbuck');
+      })
   }
+
 }
 
 module.exports = commodityController
